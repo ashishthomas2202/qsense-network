32f2b0915578b9bd81e1eaa7963d947b3aaf480e
ashishthomas2202 <98769214+ashishthomas2202@users.noreply.github.com>
2023-04-13 18:09:38 -0400

    basic signin api complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,4 +41,50 @@ exports.signup = async function (req, res) {
   }
 };
 
-exports.signin = function (req, res) {};
+exports.signin = function (req, res) {
+  console.log("Signin controller: ", req.body);
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(502).json({
+      error: {
+        path: "email",
+      },
+      message: "Email is required.",
+    });
+  } else if (!password) {
+    res.status(502).json({
+      error: {
+        path: "password",
+      },
+      message: "Password is required.",
+    });
+  } else {
+    User.findOne({ email })
+      .then((user) => {
+        console.log("user found: ", user);
+
+        let authorized = user.authenticate(password);
+
+        if (!authorized) {
+          res.status(502).json({
+            error: {},
+            message: "Invalid Email or password.",
+          });
+        } else {
+          user.hashedPassword = undefined;
+          user.salt = undefined;
+          user.__v = undefined;
+
+          res.json({ user: user, message: "Signed In Successfully" });
+        }
+      })
+      .catch((err) => {
+        console.log("error finding user: ", err);
+        res.status(502).json({
+          error: err,
+          message: "Invalid Email or password.",
+        });
+      });
+  }
+};
