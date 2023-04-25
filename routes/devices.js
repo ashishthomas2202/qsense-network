bb409a5c7c77508482ba0f060e0ceb4b8898f176
ashishthomas2202 <98769214+ashishthomas2202@users.noreply.github.com>
2023-04-25 16:45:27 -0400

    changed the device update method to put instead of post

--- routes/devices.js
+++ routes/devices.js
@@ -5,6 +5,6 @@ const { all, add, update } = require("../controllers/devices");
 
 router.get("/:userId/:accountId/all", all);
 router.post("/:userId/:accountId/add", add);
-router.post("/:userId/:accountId/:deviceId/update", update);
+router.put("/:userId/:accountId/:deviceId/update", update);
 
 module.exports = router;
